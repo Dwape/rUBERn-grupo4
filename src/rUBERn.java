8ce3d46ff4af04b155624a41516fcfbdfc8c8d77
Dwape <eduardo.lalor@ing.austral.edu.ar>
2016-10-12 19:12:05 -0300

    Created objects to be loaded when main is executed

--- src/rUBERn.java
+++ src/rUBERn.java
@@ -3,7 +3,26 @@
  */
 public class rUBERn {
     public static void main(String[] args) {
-      mainMenu();
+        MainSystem rUBERnSystem = new MainSystem();
+
+        Basic basic = new Basic();
+        Standard standard = new Standard();
+        Premium premium = new Premium();
+        Coordinates coordinates1 = new Coordinates(100, 545);
+        Coordinates coordinates2 = new Coordinates(230, 155);
+        Car car1 = new Car(4, standard, coordinates1);
+        Car car2 = new Car(3, premium, coordinates2);
+        Schedule schedule1 = new Schedule();
+        schedule1.setHoursPerDay("08:00", "16:00", "friday");
+        Schedule schedule2 = new Schedule();
+        schedule1.setHoursPerDay("10:00", "20:00", "friday");
+        Driver driver1 = new Driver(car1, "Eduardo", schedule1);
+        Driver driver2 = new Driver(car2, "Pepe", schedule2);
+
+        rUBERnSystem.addDriver(driver1);
+        rUBERnSystem.addDriver(driver2);
+
+        mainMenu();
     }
 
     static public void mainMenu(){
